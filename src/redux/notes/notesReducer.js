e1058520e2b8cb4f7b9baac0cfdb30c56b756ccc
Manoj <manoj09633@gmail.com>
2023-09-18 17:12:56 +0530

    Created notes reducer

---
+++ src/redux/notes/notesReducer.js
@@ -0,0 +1,24 @@
+import { ADD_NOTE, REMOVE_NOTE } from "./notesTypes";
+
+const initialState = {
+  notes: [
+    {
+      id: undefined,
+      title: "",
+      status: "",
+    },
+  ],
+};
+
+const notesReducer = (state=initialState, action) {
+    switch(action.type) {
+        case ADD_NOTE: 
+        console.log(action.payload)
+        return {...state};
+        case REMOVE_NOTE: console.log(action.payload)
+        return {...state};
+        default: return state;
+    }
+}
+
+export default notesReducer;
